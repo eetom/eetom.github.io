58a916544851fda2555147b561e641008bc1b99d
Eric Tsai <etsai@Erics-MacBook-Pro-475.local>
2017-12-11 16:26:57 -0800

    edit style, add time/pause button

--- todolist/action.js
+++ todolist/action.js
@@ -11,6 +11,20 @@
     myNodelist[i].appendChild(span);
   }
 
+  // Create a "timer" button and append it to each list item
+  var myNodelist2 = document.getElementsByTagName("LI");
+  var i;
+  for (i = 0; i < myNodelist2.length; i++) {
+    var timer = document.createElement("SPAN");
+    var timertxt = document.createTextNode("Pause");
+    var timeicon = document.createElement("i");
+    timeicon.className = "fa fa-clock-o";
+    timer.className = "pause";
+    timer.appendChild(timeicon);
+    timer.appendChild(timertxt);
+    myNodelist2[i].appendChild(timer);
+  }
+
   // Click on a close button to hide the current list item
   var close = document.getElementsByClassName("close");
   var i;
@@ -18,6 +32,19 @@
     close[i].onclick = function() {
       var div = this.parentElement;
       div.style.display = "none";
+    }
+  }
+
+  // Click on a timer button to hide the current list item
+  var timer2 = document.getElementsByClassName("timer");
+  var i;
+  for (i = 0; i < timer2.length; i++) {
+    timer2[i].onclick = function() {
+      if (this.className === 'pause'){
+        this.className = 'start';
+      } else {
+        this.className = 'pause';
+      }
     }
   }
 
@@ -44,16 +71,37 @@
     }
     document.getElementById("myInput").value = "";
 
+    var timer = document.createElement("SPAN");
+    var timertxt = document.createTextNode("Start Timer");
+    timer.className = "pause timer";
+    timer.appendChild(timertxt);
+    li.appendChild(timer);
+
+    for (i = 0; i < timer2.length; i++) {
+      timer2[i].onclick = function() {
+      if (this.className === 'pause timer'){
+        this.className = 'start timer';
+        this.innerHTML = '<i class="fa fa-hourglass-start" aria-hidden="true"></i> Timing 00:00';
+      } else {
+        this.className = 'pause timer';
+        this.innerHTML = '<i class="fa fa-ban" aria-hidden="true"></i> Pause 00:00';
+      }
+        console.log('TIMER START!')
+      }
+    }
+
     var span = document.createElement("SPAN");
-    var txt = document.createTextNode("\u00D7");
+    var closeicon = document.createElement("i");
+    closeicon.className = "fa fa-times";
     span.className = "close";
-    span.appendChild(txt);
+    span.appendChild(closeicon);
     li.appendChild(span);
 
     for (i = 0; i < close.length; i++) {
       close[i].onclick = function() {
         var div = this.parentElement;
         div.style.display = "none";
+        console.log('CLOSED!')
       }
     }
   }
