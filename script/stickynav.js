43789f54797fec690faf3653a66622bc319f849b
Eric Tsai <erictsaiphoto@gmail.com>
2024-01-02 15:19:06 -0800

    more updates to work and work detail page

--- script/stickynav.js
+++ script/stickynav.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
 
+  var $window = $(window),
+  $stickyEl = $('#the-sticky-div'),
+  $stickyE2 = $('#sticky-description'),
+  elTop = $stickyEl.offset().top;
 
-  // Cache selectors outside callback for performance.
-        var $window = $(window),
-        $stickyEl = $('#the-sticky-div'),
-        elTop = $stickyEl.offset().top;
-
-        $window.scroll(function () {
-            $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-    });
-
-    // Your code in here.
+  $window.scroll(function () {
+    $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+    $stickyE2.toggleClass('line-clamp-3', $window.scrollTop() > elTop);
+  });
 }); 
