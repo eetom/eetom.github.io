888dfebbc428e4024bd38be3cbdd3fcd56eabe96
Eric Tsai <erictsaiphoto@gmail.com>
2024-01-16 13:31:37 -0800

    create work specific header footer js

---
+++ work/0075601974fe1ff0a75d759526f0d6b2a6b8b1fd/headerfooterload-work.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+    // Allow for random header image
+    $('#footer').load('footer-work.html');
+    $('#header').load('.header-work.html');
+    
+    var images = [
+        'https://eetom.github.io/img/hosted/pano1.jpg',
+        'https://eetom.github.io/img/hosted/pano2.jpg',
+        'https://eetom.github.io/img/hosted/pano3.jpg',
+        'https://eetom.github.io/img/hosted/pano4.jpg',
+        'https://eetom.github.io/img/hosted/pano5.jpg',
+        'https://eetom.github.io/img/hosted/pano6.jpg',
+        'https://eetom.github.io/img/hosted/pano7.jpg',
+        'https://eetom.github.io/img/hosted/pano8.jpg',
+        'https://eetom.github.io/img/hosted/pano9.jpg',
+        'https://eetom.github.io/img/hosted/pano10.jpg',
+        'https://eetom.github.io/img/hosted/pano11.jpg',
+        'https://eetom.github.io/img/hosted/pano12.jpg',
+        'https://eetom.github.io/img/hosted/pano13.jpg',
+        'https://eetom.github.io/img/hosted/pano14.jpg',
+        'https://eetom.github.io/img/hosted/pano15.jpg',
+        'https://eetom.github.io/img/hosted/pano16.jpg',
+        'https://eetom.github.io/img/hosted/pano17.jpg',
+        'https://eetom.github.io/img/hosted/pano18.jpg',
+        'https://eetom.github.io/img/hosted/pano19.jpg',
+        'https://eetom.github.io/img/hosted/pano20.jpg'];
+
+    document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+}
+
