9b597a20ff365f44a1f462df19a4e973d90ee11f
Eric Tsai <erictsaiphoto@gmail.com>
2024-02-15 16:59:26 -0800

    test out change

--- script/stickynav.js
+++ script/stickynav.js
@@ -3,13 +3,13 @@ $(document).ready(function () {
   var $window = $(window),
   $stickyEl = $('#the-sticky-div'),
   $stickyE2 = $('#sticky-description'),
-  $stickyE3 = $('#banner'),
+  // $stickyE3 = $('#banner'),
   elTop = $stickyEl.offset().top;
 
   $window.scroll(function () {
     $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
     $stickyE2.toggleClass('line-clamp-3', $window.scrollTop() > elTop);
-    $stickyE3.toggleClass('hidden-on-scroll', $window.scrollTop() > elTop);
+    // $stickyE3.toggleClass('hidden-on-scroll', $window.scrollTop() > elTop);
   });
   
 }); 
