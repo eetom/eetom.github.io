c9efeab58dacaac65a011fd4ae5d5d89f4dc41aa
Eric Tsai <erictsaidesign@gmail.com>
2026-06-02 01:21:08 -0700

    fix image zoom pop up

--- script/imageexpand.js
+++ script/imageexpand.js
@@ -1,23 +1,64 @@
 $(document).ready(function () {
-    var modal = document.getElementById("popup1");
-    var images = document.getElementsByClassName("myImg");
-    var modalImg = document.getElementById("img01");
+    var images = Array.from(document.getElementsByClassName("myImg"));
+    var currentIndex = 0;
 
-    for (var i = 0; i < images.length; i++) {
-        var img = images[i];
-        img.onclick = function (evt) {
-            console.log(evt);
-            modal.style.visibility = "visible";
-            modalImg.src = this.src;
-        };
+    // Inject nav buttons into the existing hardcoded modal
+    if ($('#popup1').length && !$('#popup-prev').length) {
+        $('#popup1').append(
+            '<button class="popup-nav popup-prev" id="popup-prev"><i class="material-icons">chevron_left</i></button>' +
+            '<button class="popup-nav popup-next" id="popup-next"><i class="material-icons">chevron_right</i></button>'
+        );
     }
 
-    $(".popupHolder").click(function () {
-        modal.style.visibility = "hidden";
+    function showImage(index) {
+        currentIndex = index;
+        var modal = document.getElementById("popup1");
+        var modalImg = document.getElementById("img01");
+        if (modal && modalImg) {
+            modalImg.src = images[index].src;
+            modal.style.visibility = "visible";
+        }
+    }
+
+    function hideModal() {
+        var modal = document.getElementById("popup1");
+        if (modal) modal.style.visibility = "hidden";
+    }
+
+    images.forEach(function (img, i) {
+        img.onclick = function () {
+            showImage(i);
+        };
     });
 
-    $(".popupCnt").click(function (event) {
-        event.stopPropagation();
-        modal.style.visibility = "hidden";
+    $(document).on("click", "#popup-prev", function (e) {
+        e.stopPropagation();
+        showImage((currentIndex - 1 + images.length) % images.length);
     });
-}); 
+
+    $(document).on("click", "#popup-next", function (e) {
+        e.stopPropagation();
+        showImage((currentIndex + 1) % images.length);
+    });
+
+    $(document).on("click", ".popupHolder", function () {
+        hideModal();
+    });
+
+    $(document).on("click", ".popupCnt", function (e) {
+        e.stopPropagation();
+    });
+
+    $(document).keydown(function (e) {
+        var modal = document.getElementById("popup1");
+        if (modal && modal.style.visibility === "visible") {
+            if (e.key === "ArrowLeft") {
+                showImage((currentIndex - 1 + images.length) % images.length);
+            } else if (e.key === "ArrowRight") {
+                showImage((currentIndex + 1) % images.length);
+            } else if (e.key === "Escape") {
+                hideModal();
+            }
+        }
+    });
+});
